e15d1a7111bd7afb5cef6ab69236f240934aac70
Akshay.Jain <akshay.jain.7983@gmail.com>
2024-02-04 14:28:16 +0530

    initial

--- spp-portfolio-model/src/main/java/spp/portfolio/model/definition/configuration/rules/RankingRule.java
+++ spp-portfolio-model/src/main/java/spp/portfolio/model/definition/configuration/rules/RankingRule.java
@@ -1,11 +1,9 @@
 package spp.portfolio.model.definition.configuration.rules;
 
-import java.util.Collection;
-
 import lombok.Data;
 
 @Data
 public class RankingRule implements PortfolioRule
 {
-    private Collection<Ranker> rankers;
+    private Ranker ranker;
 }
